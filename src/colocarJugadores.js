c998e721ff498cc5df844efda77ab3f3c47bf2ed
Jose Contreras <felix58jose@gmail.com>
2024-02-17 14:18:20 -0600

    Solucionando problema de botones back y rules

--- src/colocarJugadores.js
+++ src/colocarJugadores.js
@@ -3,11 +3,11 @@ const colocarJugadores=(jugador1,jugador2)=>{
     document.querySelector('.jugadores').innerHTML=`
     <div class="jugador1">
         <button class="jugador--boton ${jugador1}" id="${jugador1}"><img src="images/icon-${jugador1}.svg" alt=""></button>
-        <span>Elegiste</span>
+        <span>You chose</span>
     </div>
     <div class="jugador1">
         <button class="jugador--boton ${jugador2}" id="${jugador2}"><img src="images/icon-${jugador2}.svg" alt=""></button>
-        <span>La casa eligio</span>
+        <span>Pc chose</span>
     </div>
     `;
 }
